91f30054f966a1e1dee663f2772e7ca6f75a1559
heiyanbin <heiyanbin@163.com>
2025-10-28 18:32:11 +0800

    change to neworld sub

--- api/sub/index.js
+++ api/sub/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/api/sub', async (req, res) => {
-    const url = "https://sub3.smallstrawberry.com/api/v1/client/subscribe?token=01e22a23040d1dd7b60c57ddb645213c"
+    //const url = "https://sub3.smallstrawberry.com/api/v1/client/subscribe?token=01e22a23040d1dd7b60c57ddb645213c"
+    const url = "https://blogzone.me/link/Gzx1eQIbsYnc3wTY2co0SkrcgdX8oeA6x6cY05qvQzHRtr1IjNerHBH?clash=1"
     const r = await fetch(url)
     const text = await r.text()
     res.status(r.status).send(text)
